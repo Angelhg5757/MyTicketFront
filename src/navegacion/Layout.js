8a62dd34d4fa80d479443c217cb80972a85bb3e8
Angel Hernandez Gomez <angelhg1405@gmail.com>
2023-05-31 17:56:14 -0600

    layout ocultar-mostrar

--- src/navegacion/Layout.js
+++ src/navegacion/Layout.js
@@ -5,11 +5,24 @@ import { FaTicketAlt } from 'react-icons/fa';
 import './css/navbar.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTicket } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
 
 
 function CollapsibleExample() {
-  return (
+  const [loggedIn, setLoggedIn] = useState(false);
+  
+  useEffect(() => {
+    const idUsuario = localStorage.getItem("idUsuario");
+    const nombre = localStorage.getItem("nombre");
+    // Verificar si existen los datos requeridos en localStorage
+    if (idUsuario && nombre) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  }, []);
 
+  return (
     <Navbar className="navbar-dark custom-navbar bg-dark" expand="lg">
       <Navbar.Brand className="logo" href="/inicio">
         <FontAwesomeIcon icon={faTicket} beat color="white" className="icono" />
@@ -19,21 +32,20 @@ function CollapsibleExample() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#">Conciertos</Nav.Link>
-          <Nav.Link href="#">Festivales</Nav.Link>
-          <Nav.Link href="#">Deportes</Nav.Link>
-          <Nav.Link href="#">Teatro</Nav.Link>
-          <Nav.Link href="#">Familiares</Nav.Link>
-          <Nav.Link href="#">Cultura</Nav.Link>
+          {/* <Nav.Link href="#proximos-eventos">Proximos eventos</Nav.Link>
+          <Nav.Link href="#">Eventos</Nav.Link> */}
         </Nav>
         <Nav className="inicio-sesion justify-content-between">
-        {/* <form class="d-flex">
-          <input class="form-control form-busca mr-sm-2" type="search" placeholder="Buscar por evento, artista..." aria-label="Search"/>
-            <button class="btn btn-outline-danger my-2 my-sm-0 boton-buscar" type="submit">Buscar</button>
-        </form> */}
-          <Nav.Link href="/login">
-            {/* <FaTicketAlt /> Iniciar sesión */}
-            <FontAwesomeIcon icon={faTicket} bounce color="white" className="icono" /> Iniciar sesión
+          <Nav.Link href={loggedIn ? "/profile" : "/login"}>
+          {loggedIn ? (
+            <span>
+              <FontAwesomeIcon icon={faTicket} bounce color="white" className="icono" /> Perfil
+            </span>
+          ) : (
+            <span>
+              <FontAwesomeIcon icon={faTicket} bounce color="white" className="icono" /> Iniciar sesión
+            </span>
+          )}
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
